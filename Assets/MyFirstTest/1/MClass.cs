72b93a12a03814a6e4f9234e7eed6ce64b33e5f4
WangXu <799297234@qq.com>
2017-01-11 23:53:02 +0800

    xluaTestFirstCommit

---
+++ Assets/MyFirstTest/1/MClass.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using XLua;
+
+[XLua.CSharpCallLua]
+public struct parma{
+	public int x;
+	public int y;
+}
+
+[XLua.CSharpCallLua]
+public delegate void SetIntDelegate(int i);
+
+[CSharpCallLua]
+public interface ins{
+	int a{ get; set;}
+	int b{ get; set;}
+}
+
+[XLua.LuaCallCSharp]
+public class MClass {
+	public void Test(){
+		Debug.Log ("我是lua调用的方法");
+	}
+
+	public void SetParma(parma test,SetIntDelegate fuction,ins test2)
+	{
+		Debug.Log ("CSharp test.x=" + test.x);
+		fuction (1);
+		test2.a= 10;
+	}
+}
---
+++ Assets/MyFirstTest/1/test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using XLua;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		LuaEnv luaenv = new LuaEnv();
+		luaenv.DoString("require 'runLua'");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
